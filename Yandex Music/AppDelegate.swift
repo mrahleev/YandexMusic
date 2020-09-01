6be915b91588a629cf96f4f6ded27c6a3779494d
Maksim Rahleev <Maksim.Rahleev@life.com.by>
2020-09-01 21:16:21 +0300

    Initial commit

---
+++ Yandex Music/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Yandex Music
+//
+//  Created by Maksim Rakhleyeu on 9/1/20.
+//  Copyright © 2020 Maksim Rakhleyeu. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
